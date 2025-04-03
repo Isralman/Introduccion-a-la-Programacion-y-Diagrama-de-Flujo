7a43ebd4ff96a0570f96eb7804dceb37088c4118
Isralman <Batistaquo@gmail.com>
2025-04-02 20:28:35 -0400

    This is a comit

---
+++ Funciones_con_parametros.py
@@ -0,0 +1,15 @@
+
+#Suma con funciones
+def agregar(a,b):
+    return a + b
+
+print(agregar(5,6))
+
+#Multiplicacion con funciones
+
+def multiplicar(x,y):
+    return x*y
+
+resultado = multiplicar(5,10)
+
+print(resultado)
